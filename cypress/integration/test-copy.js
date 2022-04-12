78cca39e30e54486e9e6e2cd0a96d1026dac0480
geonnia <oksana.wonderland@gmail.com>
2022-04-12 22:14:42 +0200

    added verification of all elements presence

--- cypress/integration/test-copy.js
+++ cypress/integration/test-copy.js
@@ -3,32 +3,42 @@ import LoginPage from '../pages/Login-page';
 
 /// <reference types = "cypress" />
 
+const noSuchAccountErrorMessage = "No account found with that username."
+const emptyUsernameInputErrorMessage = "Please enter username."
+const emptyPasswordInputErrorMessage = "Please enter your password."
+const wrongPasswordErrorMessage = "The password you entered was not valid."
+
 describe('Verify Login Form', () => {
 
     beforeEach(function(){
         LoginPage.visitLoginPage();
     })
 
+    it('Verify that all the elements are present on the page.', () => {
+        LoginPage.checkPageElememtsVisibility()
+        
+    });
+
     it('Try to login with unvalid credentials', () => {
         LoginPage.typeUsername(Cypress.env("username"))
         LoginPage.typePassword(Cypress.env("password"))
         LoginPage.submitForm()
-        LoginPage.elements.errorMessageUsername().should('have.text', "No account found with that username.")
+        LoginPage.elements.errorMessageUsername().should('have.text', noSuchAccountErrorMessage)
         
     });
     it('Trying to login with empty text inputs', () => {
         LoginPage.typeUsername(' ')
         LoginPage.typePassword(' ')
         LoginPage.submitForm()
-        LoginPage.elements.errorMessageUsername().should('have.text', "Please enter username1.")
-        LoginPage.elements.errorMessagePassword().should('have.text', "Please enter your password.")
+        LoginPage.elements.errorMessageUsername().should('have.text', emptyUsernameInputErrorMessage)
+        LoginPage.elements.errorMessagePassword().should('have.text', emptyPasswordInputErrorMessage)
     });
 
     it('Try to login with unvalid password', () => {
         LoginPage.typeUsername(Cypress.env("validUsername"))
         LoginPage.typePassword(Cypress.env("password"))
         LoginPage.submitForm()
-        LoginPage.elements.errorMessagePassword().should('have.text', "The password you entered was not valid.")
+        LoginPage.elements.errorMessagePassword().should('have.text', wrongPasswordErrorMessage)
         
     });
 })
--- cypress/pages/Login-page.js
+++ cypress/pages/Login-page.js
@@ -3,11 +3,22 @@ class LoginPage{
         cy.visit(Cypress.env("baseUrl"))
     }
     elements = {
+        logoImg: () => cy.get('#logomini'),
+        pageTitle: () => cy.get('h1'),
         usernameInput: () =>  cy.get('[placeholder = Username]'),
         passwordInput: () =>  cy.get('[placeholder = Password]'),
         loginButton: () =>  cy.get('[value = Login]'),
         errorMessageUsername: () =>  cy.get('.has-error [placeholder = Username] + .help-block'),
         errorMessagePassword: () =>  cy.get('.has-error [placeholder = Password] + .help-block')
+    }
+
+    checkPageElememtsVisibility(){
+        this.elements.logoImg().should('be.visible')
+        this.elements.pageTitle().should('be.visible')
+        this.elements.usernameInput().should('be.visible')
+        this.elements.passwordInput().should('be.visible')
+        this.elements.loginButton().should('be.visible')
+        
     }
 
     typeUsername(username){
